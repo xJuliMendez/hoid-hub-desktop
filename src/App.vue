1fa70ab9b217b6c926d416404bdedb1e741d6bdf
julian.mendez <you@example.com>
2023-11-07 22:57:12 +0100

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const foo = ref(1);
+const sumar = () => foo.value++;
+</script>
+
+<template>
+  <div>
+    Hello world!
+    <v-btn @click="sumar">
+      Button
+    </v-btn>
+    {{ foo }}
+  </div>
+</template>
+
